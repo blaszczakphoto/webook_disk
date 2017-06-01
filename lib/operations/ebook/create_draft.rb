9206af66322f6468d97a8fecb995fe59fa72d6a8
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-06-01 11:24:11 +0200

    refactor test

--- lib/operations/ebook/create_draft.rb
+++ lib/operations/ebook/create_draft.rb
@@ -2,15 +2,22 @@ require_relative '../../../receive_books.rb'
 
 module Ebook
   class CreateDraft
-    attr_accessor :book_id
-    def initialize(book_id:)
+    attr_accessor :book_id, :text, :toc, :book_opf
+
+    def initialize(book_id:, text:, toc:, book_opf:)
       @book_id = book_id
+      @text = text
+      @toc = toc
+      @book_opf = book_opf
     end
 
     def call
       book_stamp = book_id.to_s + '_' +  Time.now.strftime("%H%M%S_%d%m%Y")
       draft_path = "#{Sinatra::Application.settings.root}/books_drafts/#{book_stamp}"
       FileUtils::mkdir_p draft_path
+      File.open("#{draft_path}/text.html", "w") {|f| f.write(text) }
+      File.open("#{draft_path}/toc.html", "w") {|f| f.write(toc) }
+      File.open("#{draft_path}/book.opf", "w") {|f| f.write(book_opf) }
     end
   end
 end
--- spec/lib/operations/ebook/create_draft_spec.rb
+++ spec/lib/operations/ebook/create_draft_spec.rb
@@ -3,21 +3,28 @@ require 'fileutils'
 require 'timecop'
 
 describe Ebook::CreateDraft do
+  let(:root) { Sinatra::Application.settings.root }
   before do
     Timecop.freeze(Time.local(1991, 5, 11, 10, 30, 0))
   end
+  let(:expected_book_stamp) { "456_103000_11051991" }
   after do
     Timecop.return
-    FileUtils.rm_rf("#{Dir.pwd}/books_drafts")
-    FileUtils.mkdir_p("#{Dir.pwd}/books_drafts")
+    FileUtils.rm_rf("#{root}/books_drafts")
+    FileUtils.mkdir_p("#{root}/books_drafts")
   end
+  let(:text) { File.open("#{root}/spec/support/book_files/text.html").read }
+  let(:toc) { File.open("#{root}/spec/support/book_files/toc.html").read }
+  let(:book_opf) { File.open("#{root}/spec/support/book_files/book.opf").read }
+  subject { described_class.new(book_id: 456, text: text, toc: toc, book_opf: book_opf) }
 
   it "should create a new dir in books_drafts", focus: true do
-    described_class.new(book_id: 456).call
-    expect(Dir.exists?("#{Dir.pwd}/books_drafts/456_103000_11051991")).to be_truthy
+    subject.call
+    expect(Dir.exists?("#{root}/books_drafts/#{expected_book_stamp}")).to be_truthy
   end
 
-  xit "creates book files in book dir" do
-    
+  it "creates book files in book dir" do
+    subject.call
+    expect(File.exists?("#{root}/books_drafts/#{expected_book_stamp}/text.html")).to be_truthy
   end
 end
