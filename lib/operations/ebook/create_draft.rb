cd1b15180f24b15ee3171abe703a4a6d4d64909c
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-06-09 12:14:31 +0200

    copy images

--- lib/operations/ebook/create_draft.rb
+++ lib/operations/ebook/create_draft.rb
@@ -1,20 +1,25 @@
 require_relative '../../../receive_books.rb'
+require "open-uri"
+require 'uri'
 
 module Ebook
   class CreateDraft
-    attr_accessor :book_stamp, :text, :toc, :book_opf, :draft_path
+    attr_accessor :book_stamp, :text, :toc, :book_opf, :draft_path, :image_urls
 
-    def initialize(book_stamp:, text:, toc:, book_opf:, draft_path:)
+    def initialize(book_stamp:, text:, toc:, book_opf:, draft_path:, image_urls:)
       @book_stamp = book_stamp
       @text = text
       @toc = toc
       @book_opf = book_opf
       @draft_path = draft_path
+      @image_urls = image_urls
     end
 
     def call
       create_book_draft_dir
       create_book_draft_files
+      create_images_dir
+      copy_images
     end
 
     private
@@ -31,6 +36,19 @@ module Ebook
 
     def save_file_from_string(file_name, string)
       File.open("#{draft_path}/#{file_name}", "w") {|f| f.write(string) }
+    end
+
+    def create_images_dir
+      FileUtils::mkdir_p "#{draft_path}/images"
+    end
+
+    def copy_images
+      image_urls.each do |url|
+        uri = URI.parse(url)
+        File.open("#{draft_path}/images/#{File.basename(uri.path)}", 'wb') do |fo|
+          fo.write open(url).read 
+        end
+      end
     end
   end
 end
--- spec/lib/operations/ebook/create_draft_spec.rb
+++ spec/lib/operations/ebook/create_draft_spec.rb
@@ -17,14 +17,21 @@ describe Ebook::CreateDraft do
   let(:text) { File.open("#{root}/spec/support/book_files/text.html").read }
   let(:toc) { File.open("#{root}/spec/support/book_files/toc.html").read }
   let(:book_opf) { File.open("#{root}/spec/support/book_files/book.opf").read }
+  let(:image_urls) do
+    [
+      "http://jakoszczedzacpieniadze.pl/wp-content/uploads/2017/05/FB-Jak-tworzyc-zarabiajace-produkty.png",
+      "https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg",
+    ]
+  end
   subject do 
     described_class.new(
       book_stamp: book_stamp, 
       text: text, 
       toc: toc, 
       book_opf: book_opf, 
-      draft_path: draft_path
-    )
+      draft_path: draft_path,
+      image_urls: image_urls,
+      )
   end
 
   it "should create a new dir in books_drafts" do
@@ -37,5 +44,16 @@ describe Ebook::CreateDraft do
     expect(File.exists?("#{root}/books_drafts/#{book_stamp}/text.html")).to be_truthy
     expect(File.exists?("#{root}/books_drafts/#{book_stamp}/toc.html")).to be_truthy
     expect(File.exists?("#{root}/books_drafts/#{book_stamp}/book.opf")).to be_truthy
+  end
+
+  it "creates a directory for images" do
+    subject.call
+    expect(Dir.exists?("#{root}/books_drafts/#{book_stamp}/images")).to be_truthy
+  end
+
+  it "copies images" do
+    subject.call
+    expect(File.exists?("#{root}/books_drafts/#{book_stamp}/images/FB-Jak-tworzyc-zarabiajace-produkty.png")).to be_truthy
+    expect(File.exists?("#{root}/books_drafts/#{book_stamp}/images/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg")).to be_truthy
   end
 end
