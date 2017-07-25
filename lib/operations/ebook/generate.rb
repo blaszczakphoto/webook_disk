79dbc3b22d7948ac496ab2d9707210c10ac404e9
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-25 17:31:30 +0200

    spec add first

--- lib/operations/ebook/generate.rb
+++ lib/operations/ebook/generate.rb
@@ -1,6 +1,4 @@
 require_relative '../../../receive_books.rb'
-require "open-uri"
-require 'uri'
 
 module Ebook
   class Generate
@@ -12,6 +10,14 @@ module Ebook
     end
 
     def call
+      draft_file_path = "#{draft_path}/book.opf"
+      output_file_path = "#{draft_path}/#{book_id}.mobi"
+
+      stdout = `ebook-convert #{draft_file_path} #{output_file_path}`
+
+      File.open("#{Sinatra::Application.settings.root}/kindlegenlog", "a+") do |f|
+        f.write(stdout)
+      end
     end
 
     private
