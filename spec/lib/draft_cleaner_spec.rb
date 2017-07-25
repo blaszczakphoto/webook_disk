61891510ae4f82091a07b257597843d2720f632e
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-25 23:17:30 +0200

    formatig

--- spec/lib/draft_cleaner_spec.rb
+++ spec/lib/draft_cleaner_spec.rb
@@ -4,9 +4,7 @@ require 'fileutils'
 describe DraftCleaner do
   let(:book_draft_source) { "#{Sinatra::Application.settings.root}/spec/support/book_draft" }
   let(:draft_path) { "#{Sinatra::Application.settings.root}/books_drafts/12345" }
-  before do
-    FileUtils.copy_entry(book_draft_source, draft_path)
-  end
+  before { FileUtils.copy_entry(book_draft_source, draft_path) }
   subject { described_class.new(draft_path: draft_path) }
 
   it "removes draft path directory" do
