02f91cb28684ce7307f8337c20a069a5b32e6ee3
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-07-25 18:02:58 +0200

    ref spec

--- spec/lib/calibre_logger_spec.rb
+++ spec/lib/calibre_logger_spec.rb
@@ -4,17 +4,14 @@ require 'timecop'
 
 describe CalibreLogger do
   let(:kindle_log_path) { "#{Sinatra::Application.settings.root}/spec/support/kindle.log" }
-  before do
-    File.open(kindle_log_path, "w") {}
-    
-  end
-  after do
-    FileUtils.rm_f(kindle_log_path)
-  end
+  before { File.open(kindle_log_path, "w") {} }
+  after { FileUtils.rm_f(kindle_log_path) }
+
   subject { CalibreLogger.new(log_file: kindle_log_path) }
 
   it "appends text to the end of logfile" do
     subject.log("log some text")
-    expect(File.open(kindle_log_path).read).to eq("log some text")
+    log_file_content = File.open(kindle_log_path).read
+    expect(log_file_content).to eq("log some text")
   end
 end
